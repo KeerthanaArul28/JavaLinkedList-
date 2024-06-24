f029a986c1f6da34cdb2799e9545da0d515d2977
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-24 16:29:16 +0530

    getfirst

---
+++ Getfirst.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+public class Getfirst {
+    public static void main(String args[])
+    {
+        LinkedList<String>technology=new LinkedList<>();
+        technology.add("C");
+        technology.add("Java");
+        technology.add("SQL");
+        technology.add("React");
+        System.out.println(technology.getFirst());
+    }
+    
+}
