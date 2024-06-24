144cbccae7cd2ab87bf9c53e5f3f22ec3d2f3f7e
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-24 18:34:05 +0530

    size

---
+++ size.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+public class size {
+    public static void main(String args[])
+    {
+        LinkedList<String>subject=new LinkedList<>();
+        subject.add("Tamil");
+        subject.add("English");
+        subject.add("Science");
+        subject.add("Social");
+        System.out.println(subject.size());
+
+    }
+    
+}
