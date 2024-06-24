19bc27a3bc140cd276e24f7b76f67db00188b59a
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-24 16:28:13 +0530

    addlast

---
+++ addlast.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+public class addlast {
+    public static void main(String args[])
+    {
+        LinkedList<String>name=new LinkedList<>();
+        name.add("Viki");
+        name.add("Shibi");
+        name.add("Arun");
+        name.add("Harish");
+        name.addLast("Chandru");
+        System.out.println(name);
+
+
+    }
+
+    
+}
