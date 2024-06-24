259e6fa928cebd9aaca516afb3495f692ebac1d4
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-24 16:05:04 +0530

    Add files via upload

---
+++ List.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+public class List {
+    
+public static void main(String args[])
+    {
+        LinkedList<String>Colors=new LinkedList<>();
+         Colors.add("White");
+         Colors.add("Yellow");
+         Colors.add("Pink");
+         Colors.add("Blue");
+         Colors.addFirst("Green");
+         System.out.println(Colors);
+    }
+}
+    
+
+    
+
