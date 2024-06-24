696510b479394d590dcc30563881feb34cace9d1
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-24 18:30:22 +0530

    clear

---
+++ clear.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+public class clear {
+    public static void main(String args[])
+    {
+        LinkedList<String>numbers=new LinkedList<>();
+        numbers.add("one");
+        numbers.add("Two");
+        numbers.add("Three");
+        numbers.add("Four");
+        numbers.clear();
+        System.out.println(numbers);
+        numbers.add("Five");
+        numbers.add("Six");
+        System.out.println(numbers);
+
+
+
+    }
+    
+}
