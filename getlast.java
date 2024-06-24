93c6e0694a1d9c04f77c830c4c6856832db02c77
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-24 16:31:19 +0530

    getlast

---
+++ getlast.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+public class getlast {
+    public static void main(String args[])
+    {
+        LinkedList<String>colors=new LinkedList<>();
+        colors.add("Blue");
+        colors.add("white");
+        colors.add("Black");
+        colors.add("Purple");
+        System.out.println(colors.getLast());
+    }
+    
+}
