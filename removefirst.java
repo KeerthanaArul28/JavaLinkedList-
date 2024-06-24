1299df5c389861c906b23ac5551bbdf01ea207ae
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-24 18:33:11 +0530

    removefirst

---
+++ removefirst.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+public class removefirst {
+    public static void main(String args[])
+    {
+        LinkedList<String>name=new LinkedList<>();
+        name.add("abi");
+        name.add("astu");
+        name.add("viki");
+        name.add("arun");
+        name.removeFirst();
+        System.out.println(name);
+    }
+    
+}
