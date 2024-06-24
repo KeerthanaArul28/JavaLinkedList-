b45fee4d44c7de0d0192eff035c9d541e39d4092
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-24 21:51:09 +0530

    set

---
+++ set.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+public class set {
+    public static void main(String args[])
+    {
+        LinkedList<String>language=new LinkedList<>();
+        language.add("Tamil");
+        language.add("English");
+        language.add("Telugu");
+        language.add("Malayalam");
+        language.set(3,"kanada");
+        System.out.println(language);
+    }
+    
+}
