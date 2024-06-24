af6c1995c60db09f1309815e9037afa9d10ba1c6
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-24 22:11:27 +0530

    drag

---
+++ add.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+public class add {
+    public static void main(String args[])
+    {
+        LinkedList<String>numbers=new LinkedList<>();
+        numbers.add("one");
+        numbers.add("two");
+        numbers.add("three");
+        numbers.add("four");
+        numbers.add("five");
+        System.out.println(numbers);
+        numbers.clear();
+        System.out.println("After clearing the elements:");
+        numbers.add("six");
+        numbers.add("seven");
+        System.out.println(numbers);
+        System.out.println(numbers.size());
+    }
+}
