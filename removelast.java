240474c2671e93daec10b8d37c6be6b5b6fc9ce0
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-24 18:31:05 +0530

    removelast

---
+++ removelast.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+public class removelast {
+    public static void main(String args[])
+    {
+        LinkedList<String>colors=new LinkedList<>();
+        colors.add("Purple");
+        colors.add("Yellow");
+        colors.add("Pink");
+        colors.add("White");
+        colors.removeLast();
+        System.out.println(colors);
+    }
+    
+}
