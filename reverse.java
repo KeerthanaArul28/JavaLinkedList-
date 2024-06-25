75b9421e8079a7b511a57e7a8c4fea43250b2b56
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-25 16:33:59 +0530

    reverse

---
+++ reverse.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+public class reverse {
+    public static void main(String args[])
+    {
+        LinkedList<String>reverse=new LinkedList<>();
+        reverse.add("Banana");
+        reverse.add("Apple");
+        reverse.add("Mango");
+        reverse.add("Pineapple");
+        System.out.println(reverse);
+        System.out.println(reverse.reversed());
+
+
+}
+    }
+    
+
