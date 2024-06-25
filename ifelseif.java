40015912dcffb5e05cf695e313e05bcde6bc23e2
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-25 09:39:20 +0530

    ifelseif

---
+++ ifelseif.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+public class ifelseif {
+    public static void main(String args[])
+    {
+        LinkedList<String>colors=new LinkedList<>();
+        colors.add("blue");
+        colors.add("pink");
+        colors.add("purple");
+        colors.add("yellow");
+        System.out.println(colors);
+        if(colors.contains("green"))
+        {
+            System.out.println("colors is not appear");
+        }
+        else if(colors.contains("yellow"))
+        {
+            System.out.println("colors is appear");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+    }
+}
