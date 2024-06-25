f2a6c303a419b4e2a96ca48faecaea1bd708bb82
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-25 09:38:33 +0530

    condition

---
+++ condition.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+
+public class condition {
+    
+
+    public static void main(String args[])
+    {
+        LinkedList<String>name=new LinkedList<>();
+        name.add("Nandhu");
+        name.add("Lothi");
+        name.add("Kavinu");
+        name.add("Pavi");
+        System.out.println(name);
+        for(int i=0;i<name.size();i++)
+    {
+        System.out.println(name);
+    }
+    
+}
+
+    
+
+
+
+
+        
+
+    }
+    
