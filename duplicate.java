cfd1bdba3f685d902c9f06be098060d2c2b363fe
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-25 22:28:45 +0530

    duplicate

---
+++ duplicate.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class duplicate {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int arr[]=new int[a];
+        for(int i=0;i<a;i++)
+        {
+            arr[i]=sc.nextInt();
+
+        }
+        for(int i=0;i<a;i++)
+        {
+            for(int j=1;j<a;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    System.out.print(arr[i]);
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
+
+//output:
+/* 
+5
+1 2 3 4 5
+2
+*/
