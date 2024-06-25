3e3f9cace45235111c4f014e40fc10d57a222340
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-06-25 16:31:29 +0530

    count

---
+++ count.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class count {
+    
+        public static void main(String args[])
+        {
+            Scanner sc=new Scanner(System.in);
+            System.out.print("Enter the Elements:");
+            int num=sc.nextInt();
+            String a=Integer.toString(num);
+            System.out.println(a.length());
+            sc.close();
+        }
+        
+            }
+
+//count the digits
+/* 
+output:
+Enter the Elements:1575346789
+10*/
+        
+    
+    
+
