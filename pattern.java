4adade03da7fced2dbaee0a83cbfa46c72945398
KeerthanaArul28 <167212426+KeerthanaArul28@users.noreply.github.com>
2024-07-02 21:52:43 +0530

    pattern

---
+++ pattern.java
@@ -0,0 +1,29 @@
+public class pattern {
+    public static void main(String args[])
+    {
+        int n=7;
+        for(int i=0;i<n;i++,System.out.println())
+        {
+            for(int j=0;j<n;j++)
+        {
+        if(j==0||i==j||j==n-1)
+        System.out.print("* ");
+        else
+        System.out.print("  ");
+    }
+    
+}
+    }
+}
+//output
+/*
+
+*           * 
+* *         *
+*   *       *
+*     *     *
+*       *   *
+*         * *
+*           *
+
+ */
